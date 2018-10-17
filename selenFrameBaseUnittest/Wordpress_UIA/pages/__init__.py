58380b78f63f5d929924f9edf16cfb090e53222d
hawkwind1 <pansc2004@126.com>
2018-10-17 14:47:11 +0800

    remove some files

--- selenFrameBaseUnittest/Wordpress_UIA/pages/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2018/10/11 22:24
-# @Author  : PanCheng
-# @Site    : 
-# @File    : __init__.py.py
-# @Software: PyCharm
--- selenFrameBaseUnittest/Wordpress_UIA/pages/edittopicpage.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2018/10/11 22:28
-# @Author  : PanCheng
-# @Site    : 
-# @File    : edittopicpage.py
-# @Software: PyCharm
